e7308ec1205292e8ce175b5c752f1847f6cc40e9
Максим Мельников <tryhardworker@mail.ru>
2025-08-22 13:52:15 +0300

    feat: add little features

--- projects/home/nginx/frontend/src/features/filters/filters.js
+++ projects/home/nginx/frontend/src/features/filters/filters.js
@@ -615,7 +615,9 @@ document.addEventListener("DOMContentLoaded", () => {
         <div class="definition">
           <header>ЖК # ${estate.id}</header>
           <div class="description">${formattedDescription}</div>
-          <button>Узнать ЖК</button>
+          <a target="_blank" rel="noopener noreferrer" href="https://t.me/+79069270227?text=%D0%97%D0%B4%D1%80%D0%B0%D0%B2%D1%81%D1%82%D0%B2%D1%83%D0%B9%D1%82%D0%B5%20%D0%A1%D0%B5%D1%80%D0%B3%D0%B5%D0%B9%20%D0%AE%D1%80%D1%8C%D0%B5%D0%B2%D0%B8%D1%87,%20%D0%BC%D0%B5%D0%BD%D1%8F%20%D0%B7%D0%BE%D0%B2%D1%83%D1%82...%20%D0%9D%D1%83%D0%B6%D0%BD%D0%B0%20%D0%BA%D0%BE%D0%BD%D1%81%D1%83%D0%BB%D1%8C%D1%82%D0%B0%D1%86%D0%B8%D1%8F%20%D0%BF%D0%BE%20%D0%BD%D0%B5%D0%B4%D0%B2%D0%B8%D0%B6%D0%B8%D0%BC%D0%BE%D1%81%D1%82%D0%B8">
+            <button>Узнать ЖК</button>
+          </a>
         </div>
       `;
           mainBot.appendChild(div);
