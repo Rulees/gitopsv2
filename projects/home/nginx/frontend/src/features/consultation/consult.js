8306a99ee61a7eedc2b3134b24b9cb34aae98056
Максим Мельников <tryhardworker@mail.ru>
2025-11-01 00:53:40 +0300

    feat: monitoring is ready

--- projects/home/nginx/frontend/src/features/consultation/consult.js
+++ projects/home/nginx/frontend/src/features/consultation/consult.js
@@ -3,21 +3,17 @@ const popup = document.getElementById("popup-consultation");
 const closeBtnSvg = document.querySelector("#popup-consultation-close svg");
 
 let hideTimeout;
-let showTimeout;
 
 if (trigger && popup) {
   const showPopup = () => {
     clearTimeout(hideTimeout);
-    showTimeout = setTimeout(() => {
-      popup.classList.add("popup--active");
-      const rect = trigger.getBoundingClientRect();
-      popup.style.top = rect.bottom + -0 + "px";
-      popup.style.left = rect.left + "px";
-    }, 110);
+    popup.classList.add("popup--active");
+    const rect = trigger.getBoundingClientRect();
+    popup.style.top = rect.bottom + -0 + "px";
+    popup.style.left = rect.left + "px";
   };
 
   const hidePopup = () => {
-    clearTimeout(showTimeout);
     hideTimeout = setTimeout(() => {
       popup.classList.remove("popup--active");
     }, 220);
@@ -27,7 +23,6 @@ if (trigger && popup) {
   trigger.addEventListener("mouseleave", hidePopup);
 
   popup.addEventListener("mouseenter", () => {
-    clearTimeout(showTimeout);
     clearTimeout(hideTimeout);
     popup.classList.add("popup--active");
   });
@@ -35,8 +30,6 @@ if (trigger && popup) {
 
   if (closeBtnSvg) {
     closeBtnSvg.addEventListener("click", () => {
-      clearTimeout(showTimeout);
-      clearTimeout(hideTimeout);
       popup.classList.remove("popup--active");
     });
   }
